59ca6ebf9c1ff97c4b31eb5ac8d3dff976f9fffa
Jean Villalobos <djjean85@gmail.com>
2017-02-06 20:22:59 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//Add effects to btn and rearrange cols based on width
+$(document).ready(function(){ 
+	$("a.pill-btn").addClass("wow fadeInUp").attr("data-wow-duration", "1s");
+
+	if ($(window).width() < 768) {
+		$(".rearrange > div").each(function() {
+    		$(this).prependTo(this.parentNode);
+		})
+	}
+
+});
+
+
+//Smooth Scrolling
+$(document).on('click', '.scrollTo', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
+
